7a540e18c4f1d9fbb218d69445258e6436cd7330
azadov-azamat <azamatazadov112@gmail.com>
2023-10-06 03:29:47 +0500

    bug

--- src/view/sales/view.tsx
+++ src/view/sales/view.tsx
@@ -92,7 +92,7 @@ export default function ViewSales() {
             <Card className='px-4 py-3 md:w-9/12 w-full'>
                 <CardBody className='p-0'>
                 {
-                            sale?.soldproducts?.length > 0 &&   <div className="sold-product my-2">
+                            sale.soldProducts.length > 0 &&   <div className="sold-product my-2">
                             <Typography variant="paragraph">Sotilgan mahsulotlar: </Typography>
                            <div className="flex ">
                                 <div className="w-1/3 text-base font-bold">Nomi</div>
@@ -101,12 +101,12 @@ export default function ViewSales() {
                            </div>
                            <div className="">
                                 {
-                                    sale?.soldproducts?.map((pr, ip)=> {
+                                    sale?.soldProducts?.map((pr, ip)=> {
                                         return (
                                             <div className="flex relative" key={ip}>
-                                                  <div className="w-1/3">{pr.soldProductName}</div>
-                                                   <div className="w-1/3">{pr.soldPrice} sum</div>
-                                                <div className="w-1/3">{pr.soldQuantity}</div>
+                                                  <div className="w-1/3">{pr?.soldProductName}</div>
+                                                   <div className="w-1/3">{pr?.soldPrice} sum</div>
+                                                <div className="w-1/3">{pr?.soldQuantity}</div>
                                                 <div className="absolute right-0">
                                                     <FiExternalLink className="text-green-500 text-base cursor-pointer" onClick={()=> {
                                                         localStorage.setItem("mgId", String(sale?.storeId))
