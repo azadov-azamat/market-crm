075ef85fb32d2713bc41097797d223110a81e4c6
azadov-azamat <azamatazadov112@gmail.com>
2023-10-12 08:57:41 +0500

    api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const TOKEN = "magazine-seller"
 // export const baseUrl = "https://guzarpost.uz/api/v1"
-export const baseUrl = "https://marketlochin.uz/api/v1";
+export const baseUrl = "https://api.marketlochin.uz/api/v1";
 export const noIMG = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKYUV7MhggIFbp11n_GaG__uYdPIWh8wTtyt9wtgldalCnN1qMvghZ0uholPhmV2jHmHc&usqp=CAU'
 export const getToken = () => localStorage.getItem(TOKEN)
 
@@ -12,9 +12,9 @@ export const http = axios.create({
     baseURL: baseUrl,
     headers: {
         Accept: "application/json",
-        AccessControlAllowOrigin: "*",
-        AccessControlAllowCredentials: true,
-        AccessControlAllowMethods: 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
     }
 })
 
@@ -22,9 +22,9 @@ export const http_auth = axios.create({
     baseURL: baseUrl,
     headers: {
         Accept: "application/json",
-        AccessControlAllowOrigin: "*",
-        AccessControlAllowCredentials: true,
-        AccessControlAllowMethods: 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
         ContentType: "application/json",
         Authorization: getAuthorizationHeader()
     }
